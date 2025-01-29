09aec9d6aac5cd4e8576a410e2c5f3fa6d80febc
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2025-01-29 10:34:24 +0100

    Added before each for update_add

--- ts/tests/update_adress.test.ts
+++ ts/tests/update_adress.test.ts
@@ -1,15 +1,20 @@
 import { test, expect, chromium } from '@playwright/test';
 
-test('test', async () => {
-  const browser = await chromium.launch({ slowMo: 1000 });
-  const page = await browser.newPage();
-  
-  // Reset database
+test.beforeEach('init page and reset database',async () => {
+  const browser = await chromium.launch({ slowMo: 1000 })
+  const page = await browser.newPage()
+
   await page.goto('/reset_db')
   const proceedButton = page.locator("button:has-text('proceed')")
   await proceedButton.click()
+})
 
-  await page.getByRole('link', { name: 'Add new employee' }).click();
+test('Modify Address line 2', async () => {
+  const browser = await chromium.launch({ slowMo: 1000 });
+  const page = await browser.newPage();
+  
+
+  await page.goto('/add_employee')
   await page.getByPlaceholder('Name').fill('Name');
   await page.getByPlaceholder('Email').fill('email@mail.com');
   await page.locator('#id_address_line1').fill('Address');
