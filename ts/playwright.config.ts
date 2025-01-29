27f9f3667f988dcbf1c839ae1de8002e80705ea4
Zyad Zekri <72571140+BadTimeGiver@users.noreply.github.com>
2025-01-29 08:48:23 +0100

    Setup and Tested Add-Team using playwright

--- ts/playwright.config.ts
+++ ts/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: false,
   use: {
     // TODO: fix the URL
-    baseURL: 'https://<letter>.<group>.hr.dmerej.info',
+    baseURL: 'https://A.SE2.hr.dmerej.info',
 
     trace: 'on-first-retry'
   },
